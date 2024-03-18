b4f65717032637f61f193dbaa97767ac966f7247
Sherren Jie <114548511+sherrenjie@users.noreply.github.com>
2024-03-18 19:58:26 -0400

    Create 104_maxDepth.java

---
+++ 104_maxDepth.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left) + 1;
+        int rightDepth = maxDepth(root.right) + 1;
+        return Math.max(leftDepth, rightDepth);
+    }
+
+    
+}
